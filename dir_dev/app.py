8879d5b734511215186451de4cc71d962b96da1a
Rihardssss <154497544+Rihardssss@users.noreply.github.com>
2026-04-17 13:33:59 +0300

    setup dev and server

--- dir_dev/app.py
+++ dir_dev/app.py
@@ -1,4 +1,1 @@
-print("Hello")
-
-with open("dir_server/result.txt", "w") as f:
-    f.write("Program ran successfully - V2")
+print("DEV VERSION V1")
--- dir_server/app.py
+++ dir_server/app.py
@@ -1,1 +1,1 @@
-print("Hello World!")
+print("SERVER OLD VERSION")
